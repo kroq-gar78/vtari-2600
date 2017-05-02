577bcd70b77213ed9bbc3c7269194e10c6fdac83
Aditya Vaidya <kroq.gar78@gmail.com>
2017-05-01 21:10:58 -0500

    Change format of instructions

--- inst.py
+++ inst.py
@@ -20,12 +20,14 @@ d = defaultdict(set)
 addr_modes = set()
 
 opcodes_table = []
+addrmodes_table = []
 sums = 0
 
 # print
 # sys.stdout.write('F0[16][16] opcodes = {')
 for a in xrange(0x10):
-    l = []
+    l_opcodes = []
+    l_addrmodes = []
     for b in xrange(0x10):
         inst, addr = lines_sep[a][b]
         inst = inst.lower()
@@ -35,43 +37,59 @@ for a in xrange(0x10):
 
 
         if inst == "???" or "addr" == "---":
-            l.append("(F1) OPCODE_BAD")
+            l_opcodes.append("(F2) OPCODE_BAD")
+            l_addrmodes.append("-1");
             continue
 
+        if(addr == "a"): addr = "impl"
         d[inst].add(addr)
         addr_modes.add(addr)
 
-        l.append("%s_%s" % (inst, addr))
+        # l.append("%s_%s" % (inst, addr))
+        l_opcodes.append("inst_%s" % inst)
+        l_addrmodes.append("ADDRMODE_%s" % addr.upper())
     # print "row", len(l)
-    opcodes_table.append(', '.join(l))
-    sums += len(l)
+    opcodes_table.append(', '.join(l_opcodes))
+    addrmodes_table.append(', '.join(l_addrmodes))
+    sums += len(l_opcodes)
 
 opcodes_table_str = ', \n'.join(opcodes_table)
+addrmodes_table_str = ', \n'.join(addrmodes_table)
 
 print
 print
 # print(sums)
 
 for inst in sorted(d.iterkeys()):
-    for addr in sorted(d[inst]):
-        print "int %s_%s(short addr);" % (inst, addr)
+    print "short inst_%s(ushrt addr, int addr_mode);" % inst
 
-print "F1 opcodes[256] = {%s};" % opcodes_table_str
+print
+print
+
+print "F2 opcodes[256] = {%s};" % opcodes_table_str
+print
+print "int addr_modes[256] = {%s};" % addrmodes_table_str
 
 print
 print
 
 for inst in sorted(d.iterkeys()):
-    for addr in sorted(d[inst]):
-        if addr == "impl": continue
-        print "int %s_%s(short addr)" % (inst, addr)
-        print "{"
-        print "    ushrt addr_e = addr_%s(addr);" % addr
-        print "    byte val_8 = mem_get8(addr_e);"
-        if("zpg" in addr):
-            print "    byte val_16 = mem_get16_zpg(addr_e);"
-        else:
-            print "    byte val_16 = mem_get16(addr_e);"
-        print
-        print "    return 0;"
-        print "}"
+    print "int inst_%s(short addr, int addr_mode)" % inst
+    print """
+    F1 addr_f = addr_mode_f[addr_mode];
+    ushrt addr_e = addr_f(addr);
+
+    // get operands, using zero-page if necessary
+    byte val_8 = mem_get8(addr_e);
+    ushrt val_16 = 0;
+    if(addr_mode == ADDRMODE_ZPG || addr_mode == ADDRMODE_ZPG_X || addr_mode == ADDRMODE_ZPG_Y)
+    {
+        byte val_16 = mem_get16_zpg(addr_e);
+    else
+    {
+        byte val_16 = mem_get16(addr_e);
+    }
+
+    return 0;
+}"""
+
