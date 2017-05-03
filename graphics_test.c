281efb8ab5dcdc862bc09eba88a4d0be6e900f8f
Aditya Vaidya <kroq.gar78@gmail.com>
2017-05-03 03:36:57 -0500

    Basic SDL tutorial

--- graphics_test.c
+++ graphics_test.c
@@ -2,10 +2,41 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "SDL2/SDL.h"
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* argv[])
 {
+    SDL_Window* window = NULL;
+
+    SDL_Surface* screenSurface = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if(window == NULL)
+    {
+        SDL_Log("Unable to initialize SDL window: %s", SDL_GetError());
+        return 1;
+    }
+
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xff, 0xff, 0xff));
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
 #endif
--- graphics_test.sh
+++ graphics_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -o graphics_test graphics_test.c 
+gcc -o graphics_test graphics_test.c $(sdl2-config --cflags) $(sdl2-config --libs)
